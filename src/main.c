7ec07573e636b29cce792a6d5e1df4d8f798602b
Ícaro de Brito Eda <icarobritoeda@gmail.com>
2024-11-29 23:41:31 -0300

    final beta 0.0.1

--- src/main.c
+++ src/main.c
@@ -192,6 +192,7 @@ int main(int argc, char *argv[]) {
         for (size_t i = 0; i < k; i++) {
             char command[MAX_INPUT_SIZE + 20];
             snprintf(command, sizeof(command), "git clone %s", urls[i]);
+            system("cd ./src/include/");
             system(command);
         }
     }
